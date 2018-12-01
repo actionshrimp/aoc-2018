3f5db9d180b7884193976c03ec432e706f53691b
Dave Aitken <dave.aitken@gmail.com>
2018-12-01 21:19:40 +0000

    Fetching data - got a response

--- src/bin/fetch.rs
+++ src/bin/fetch.rs
@@ -1,3 +1,33 @@
+// extern crate reqwest;
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+// use reqwest;
+
 fn main() {
-    println!("Hello, fetch!");
+    let args: Vec<String> = env::args().collect();
+
+    let day = &args[1]; let dayi = day.parse::<i32>().expect("invalid day");
+
+    let mut f = File::open("config/sessionid").expect("data/sessionid file not found");
+
+    let mut sessionid = String::new();
+    f.read_to_string(&mut sessionid)
+        .expect("something went wrong reading the file");
+
+    let uri = format!("https://adventofcode.com/2018/day/{}/input", dayi);
+
+    let client = reqwest::Client::new();
+    let mut res = client
+        .get(&uri)
+        .header("Cookie", format!("session={}", &sessionid))
+        .send()
+        .expect("http error");
+
+    let body = res.text();
+
+    println!("body = {:?}", body);
+    // Ok(())
 }
