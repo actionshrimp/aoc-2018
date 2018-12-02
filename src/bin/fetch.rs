7e1c7a8804f6f8cd0259d9f11a1ea4bce6bb2416
Dave Aitken <dave.aitken@gmail.com>
2018-12-02 14:08:41 +0000

    Finished data fetching script

--- src/bin/fetch.rs
+++ src/bin/fetch.rs
@@ -3,31 +3,48 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 
 // use reqwest;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let day = &args[1]; let dayi = day.parse::<i32>().expect("invalid day");
+    let day = &args
+        .get(1)
+        .expect("no day arg passed")
+        .parse::<i32>()
+        .expect("invalid day (expected integer)");
 
-    let mut f = File::open("config/sessionid").expect("data/sessionid file not found");
+    let data_path = format!("data/{:02}.txt", day);
+    let fexists = Path::new(&data_path).exists();
 
-    let mut sessionid = String::new();
-    f.read_to_string(&mut sessionid)
-        .expect("something went wrong reading the file");
+    if fexists {
+        println!("{} already exists", &data_path);
 
-    let uri = format!("https://adventofcode.com/2018/day/{}/input", dayi);
+    } else {
+        let mut f = File::open("config/session_id").expect("data/session_id file not found");
 
-    let client = reqwest::Client::new();
-    let mut res = client
-        .get(&uri)
-        .header("Cookie", format!("session={}", &sessionid))
-        .send()
-        .expect("http error");
+        let mut session_id = String::new();
+        f.read_to_string(&mut session_id)
+            .expect("something went wrong reading the file");
 
-    let body = res.text();
+        let uri = format!("https://adventofcode.com/2018/day/{}/input", day);
 
-    println!("body = {:?}", body);
-    // Ok(())
+        let client = reqwest::Client::new();
+        let mut res = client
+            .get(&uri)
+            .header("Cookie", format!("session={}", &session_id))
+            .send()
+            .expect("http error");
+
+        let body = res.text().expect("something went wrong reading the body");
+
+        let mut f = File::create(&data_path)
+            .expect(&format!("couldnt create file: {}", data_path));
+
+        f.write_all(&body.as_bytes()).expect("error writing contents");
+
+        println!("{} written", &data_path);
+    }
 }
