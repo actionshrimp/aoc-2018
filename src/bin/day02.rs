7ed1da4aae5c40fed7b46c1a768fccc1e476706e
Dave Aitken <dave.aitken@gmail.com>
2018-12-02 19:58:50 +0000

    Day 2 part 2

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::iter::Iterator;
 
 fn counts(s: &str) -> HashMap<char, i8> {
     let mut counts = HashMap::new();
@@ -19,7 +20,7 @@ fn has(c : &HashMap<char, i8>, j : i8) -> bool {
 }
 
 fn part1(s: &str) -> i32 {
-    let (twos, threes) = s.lines().fold((0, 0), |(twos,threes), line| {
+    let (twos, threes) = s.lines().fold((0, 0), |(twos, threes), line| {
         let c = counts(line);
 
         match (has(&c, 2), has(&c, 3)) {
@@ -33,10 +34,54 @@ fn part1(s: &str) -> i32 {
     twos * threes
 }
 
+fn compare_ids(s: &str, s2: &str) -> i8 {
+    s.chars().zip(s2.chars()).fold(0, |acc, (c1, c2)| {
+        if c1 != c2 {
+            acc + 1
+        } else {
+            acc
+        }
+    })
+}
+
+fn common(s: &str, s2: &str) -> String {
+    let mut result = String::new();
+
+    s.chars().zip(s2.chars()).for_each(|(c1, c2)| {
+        if c1 == c2 {
+            result.push(c1)
+        }
+    });
+
+    result
+}
+
+fn part2(s: &str) -> String {
+    let lines : Vec<&str> = s.lines().collect();
+    let mut i = 0;
+
+    let (item1, item2) = loop {
+        let item = lines[i];
+        let rest = &lines[(&i+1)..];
+
+        let search = rest.iter().find(|item2| {
+            compare_ids(item, item2) == 1
+        });
+
+        match search {
+            | None => i += 1,
+            | Some (item2) => break (item, item2)
+        }
+    };
+
+    common(item1, item2)
+}
+
 fn main() {
     let fname = "data/02.txt";
     let fdata = std::fs::read_to_string(fname)
         .expect(&format!("couldn't read {}", fname));
 
     println!("result p1: {}", part1(&fdata));
+    println!("result p2: {}", part2(&fdata));
 }
