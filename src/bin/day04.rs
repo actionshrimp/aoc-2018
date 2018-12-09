d319fea6f0a5d7702d8f92c36989ec122c779c49
Dave Aitken <dave.aitken@gmail.com>
2018-12-09 13:27:57 +0000

    Day 4: tidy up parser

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,6 +1,6 @@
 extern crate combine;
 
-use combine::{Parser, ParseError, Stream, StreamOnce};
+use combine::{Parser, ParseError, Stream};
 use combine::parser::char::{char, digit};
 use combine::parser::repeat::*;
 use combine::{token};
@@ -15,49 +15,26 @@ enum EventEntry {
 // [1518-10-20 00:48] wakes up
 // [1518-08-12 00:14] falls asleep
 
-fn minute<I>() -> impl Parser<Input = I, Output = i32>
-where
-    I: Stream<Item = char>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    take_until::<Vec<_>,_>(token(':'))
-        .with(char(':'))
-        .with((digit(), digit()))
-        .map(|(x, y) : (char, char)| {
-            let x = x.to_digit(10).expect("digit");
-            let y = y.to_digit(10).expect("digit");
-            (x * 10 + y) as i32
-        })
-}
-
-fn parse_event<I>() -> impl Parser<Input = I, Output = EventEntry>
-where
-    I: Stream<Item = char>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    let begin = combine::parser::char::string(" Guard #")
-        .with(many1::<Vec<_>, _>(digit()).map(|d| {
-            let s : String = d.iter().collect();
-            s.parse::<i32>().unwrap()
-        }))
-        .map(|id : i32| {
-            EventEntry::Begin(id)
-        });
-
-    let wakes_up = combine::parser::char::string(" wakes up").map(|_| { EventEntry::WakesUp });
-    let falls_asleep = combine::parser::char::string(" falls asleep").map(|_| { EventEntry::FallsAsleep });
-
-    combine::choice((
-        begin,
-        wakes_up,
-        falls_asleep
-    ))
-}
-
 fn parse_line(line: &str) -> (i32, EventEntry) {
 
-    let mut parser = combine::between(combine::token('['), combine::token(']'), minute())
-        .and(parse_event());
+    let minute = take_until::<String,_>(token(':'))
+        .with(char(':'))
+        .with(many1::<String, _>(digit()))
+        .map(|s| { s.parse::<i32>().unwrap() });
+
+    let begin = combine::parser::char::string(" Guard #")
+        .with(many1::<String, _>(digit())
+              .map(|s| {s.parse::<i32>().unwrap()})
+        ).map(|id : i32| { EventEntry::Begin(id) });
+
+    let event = combine::choice((
+        begin,
+        combine::parser::char::string(" wakes up").map(|_| { EventEntry::WakesUp }),
+        combine::parser::char::string(" falls asleep").map(|_| { EventEntry::FallsAsleep })
+    ));
+
+    let mut parser = combine::between(combine::token('['), combine::token(']'), minute)
+        .and(event);
 
     let (result, _rest) = parser.parse(line).expect(&format!("parse failed: {}", &line));
 
