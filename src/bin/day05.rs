4b8c346cf79bf8d82f6fc8c20728e49c9127071b
Dave Aitken <dave.aitken@gmail.com>
2018-12-11 22:54:18 +0000

    day 5: part 2

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,13 +1,11 @@
-use std::str::Chars;
-
 fn is_inverse (c1 : char, c2 : char) -> bool {
     c1.to_ascii_lowercase() == c2.to_ascii_lowercase() && c1 != c2
 }
 
-fn run_chain(chars : Chars) -> Vec<char> {
+fn run_chain(chain : &str) -> Vec<char> {
     let mut stack = Vec::new();
 
-    for c in chars {
+    for c in chain.chars() {
         match stack.pop() {
             | None =>
                 stack.push(c),
@@ -28,6 +26,29 @@ fn test_chain() {
     assert_eq!(res.into_iter().collect::<String>(), "dabCBAcaDA");
 }
 
+fn part1(chain: &str) -> usize {
+    let result = run_chain(chain);
+    result.len()
+}
+
+fn part2(chain: &str) -> usize {
+    let mut shortest = 50000;
+    for bad in "abcdefghijklmnopqrstuvwxyz".chars() {
+        let without = chain.chars().filter(|c| {
+            c.to_ascii_lowercase() != bad
+        });
+
+        let chain_result = run_chain(&without.collect::<String>());
+        let len = chain_result.len();
+
+        if len < shortest {
+            shortest = len
+        }
+    }
+
+    shortest
+}
+
 fn main() {
     let fname = "data/05.txt";
     let fdata = std::fs::read_to_string(fname)
@@ -35,10 +56,7 @@ fn main() {
 
     let first_line = fdata.lines().collect::<Vec<_>>().into_iter().nth(0).expect("first line");
 
-    let chars = first_line.chars();
-
-    let p1_chain = run_chain(chars);
-    println!("{}", p1_chain.iter().collect::<String>());
-
-    println!("result p1: {}", p1_chain.len());
+    let chain = first_line;
+    println!("result p1: {}", part1(&chain));
+    println!("result p2: {}", part2(&chain));
 }
