3092930b7e2095e4d264f5f5e024857f36b9b6a2
Dave Aitken <dave.aitken@gmail.com>
2018-12-28 12:30:42 +0000

    day 7: split consume for part 2

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -31,31 +31,25 @@ impl Graph {
         g
     }
 
-    fn consume_first_available(&mut self) -> Option<Node> {
-        let e = self.available.iter().min().map(|v| v.clone());
+    fn remove (self : &mut Graph, n: Node) -> () {
+        self.available.remove(&n);
+        match self.node_dependents.remove_entry(&n) {
+            | None => {}
+            | Some((_, deps)) => {
+                deps.iter().for_each(|d| {
+                    self.node_requirements.entry(*d).and_modify(|v| { v.remove(&n); });
 
-        match e {
-            None => {}
-            Some(earliest) => {
-                self.available.remove(&earliest);
-                match self.node_dependents.remove_entry(&earliest) {
-                    | None => {}
-                    | Some((_, deps)) => {
-                        deps.iter().for_each(|d| {
-                            self.node_requirements.entry(*d).and_modify(|v| { v.remove(&earliest); });
-
-                            if self.node_requirements.get(d).expect("reqs for dep").len() == 0 {
-                                self.node_requirements.remove(d);
-                                self.available.insert(*d);
-                            }
-                        })
+                    if self.node_requirements.get(d).expect("reqs for dep").len() == 0 {
+                        self.node_requirements.remove(d);
+                        self.available.insert(*d);
                     }
-                }
+                })
             }
         }
 
-        e
+        ()
     }
+
 }
 
 struct Pair { node: Node, requirement: Node }
@@ -75,38 +69,70 @@ fn test_parse() {
     assert_eq!('M', parsed.node);
 }
 
+fn remove_first_available(g: &mut Graph) -> Option<Node> {
+    let e = g.available.iter().min().map(|v| v.clone());
+
+    match e {
+        None => {}
+        Some(earliest) => {
+            g.remove(earliest);
+        }
+    }
+
+    e
+}
+
 fn calc_order(mut g : Graph) -> String {
     let mut s = String::new();
 
-    while let Some(c) = g.consume_first_available() {
+    while let Some(c) = remove_first_available(&mut g) {
         s.push(c);
     }
 
     s
 }
 
-#[test]
-fn test_example() {
-    let example_input =
-        vec!["Step C must be finished before step A can begin.",
-             "Step C must be finished before step F can begin.",
-             "Step A must be finished before step B can begin.",
-             "Step A must be finished before step D can begin.",
-             "Step B must be finished before step E can begin.",
-             "Step D must be finished before step E can begin.",
-             "Step F must be finished before step E can begin."];
+#[cfg(test)]
+mod test {
+    use super::*;
 
-    let parsed = example_input.iter().map(|p| {parse_line(p)}).collect::<Vec<_>>();
-    let g = Graph::from_pairs(&parsed);
-    assert_eq!("CABDFE", calc_order(g));
+    static EXAMPLE_LINES : [&str; 7] =
+        ["Step C must be finished before step A can begin.",
+         "Step C must be finished before step F can begin.",
+         "Step A must be finished before step B can begin.",
+         "Step A must be finished before step D can begin.",
+         "Step B must be finished before step E can begin.",
+         "Step D must be finished before step E can begin.",
+         "Step F must be finished before step E can begin."];
+
+    #[test]
+    fn test_example() {
+        let example_pairs = EXAMPLE_LINES.iter().map(|p| {parse_line(p)}).collect::<Vec<_>>();
+        let g = Graph::from_pairs(&example_pairs);
+        assert_eq!("CABDFE", calc_order(g));
+    }
 }
+
 
 fn part1(pairs: &Vec<Pair>) -> String {
-
     let g = Graph::from_pairs(pairs);
     calc_order(g)
-
 }
+
+// fn calc_duration(mut g: Graph, workers: u8, base_delay: u8) -> u32 {
+//     let workers : Vec<(char, i32)> = Vec::new();
+// }
+
+// #[test]
+// fn test_example_part_2() {
+//     let g = Graph::from_pairs(&example_pairs);
+//     assert_eq!(15, calc_duration(g, 2, 0));
+// }
+
+// fn part2(pairs: &Vec<Pair>) -> u32 {
+//     let g = Graph::from_pairs(pairs);
+//     calc_duration(g, 5, 60)
+// }
 
 fn main() {
     let fname = "data/07.txt";
